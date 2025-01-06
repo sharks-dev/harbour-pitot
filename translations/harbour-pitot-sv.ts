30b549146a6bc40524132972d1bcdbb989d31d46
Åke Engelbrektson <eson57@users.noreply.github.com>
2025-01-06 08:44:46 +0100

    Add files via upload
    Swedish translation

---
+++ translations/harbour-pitot-sv.ts
@@ -0,0 +1,72 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="sv">
+<context>
+    <name>AboutPage</name>
+    <message>
+        <source>About</source>
+        <translation>Om</translation>
+    </message>
+    <message>
+        <source>Open project website in browser</source>
+        <translation>Öppna projektsidan i webbläsaren</translation>
+    </message>
+    <message>
+        <source>Pitot is open source software. You can find the licence details and source code on GitHub.</source>
+        <translation>Pitot är programvara med öppen källkod. Du hittar licensinformation och källkod på GitHub.</translation>
+    </message>
+</context>
+<context>
+    <name>MainPage</name>
+    <message>
+        <source>Settings</source>
+        <translation>Inställningar</translation>
+    </message>
+    <message>
+        <source>About</source>
+        <translation>Om</translation>
+    </message>
+    <message>
+        <source>Getting location. Please ensure location services are enabled…</source>
+        <translation>Hämtar plats! Tillse att platstjänster är aktiverat...</translation>
+    </message>
+</context>
+<context>
+    <name>SettingsPage</name>
+    <message>
+        <source>Display unit</source>
+        <translation>Visningsenhet</translation>
+    </message>
+    <message>
+        <source>Settings</source>
+        <translation>Inställningar</translation>
+    </message>
+</context>
+<context>
+    <name>harbour-pitot</name>
+    <message>
+        <source>meters per second</source>
+        <translation>meter per sekund</translation>
+    </message>
+    <message>
+        <source>meters per minute</source>
+        <translation>meter per minut</translation>
+    </message>
+    <message>
+        <source>kilometers per hour</source>
+        <translation>kilometer per timma</translation>
+    </message>
+    <message>
+        <source>miles per hour</source>
+        <translation>miles per timma</translation>
+    </message>
+    <message>
+        <source>feet per second</source>
+        <translation>fot per sekund</translation>
+    </message>
+    <message>
+        <source>knots</source>
+        <translation>knop</translation>
+    </message>
+</context>
+</TS>
